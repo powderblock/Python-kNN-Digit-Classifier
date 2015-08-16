ee94d2457358bb6a52f4f975da87e5f55768f9bd
Blade Nelson <powderblock@gmail.com>
2015-08-16 19:05:35 -0400

    Add more digits, update python code
    Add more digits. Update the python code to actually go through all the
    images and train a dataset.

--- digit_classifier.py
+++ digit_classifier.py
@@ -4,6 +4,7 @@ import re
 import numpy as np
 import cv2
 import sys
+from collections import *
 
 def distance(a, b):
     return sqrt(sum((a - b) ** 2 for a, b in zip(a, b)))
@@ -15,49 +16,31 @@ def classify(a, dataset):
     return sorted(distances)[0][1]
 
 images = []
-for i in range(5):
-    print "zero{0}.png".format(i)
-    img = cv2.imread("digits/zero{0}.png".format(i))
 
-    width = img.shape[0]
-    height = img.shape[1]
+digits = OrderedDict((("zero", 0), ("one", 1), ("two", 2), ("three", 3), ("four", 4), ("five", 5), ("six", 6), ("seven", 7), ("eight", 8), ("nine", 9)))
 
-    pixels = []
+for value in digits:
+    for i in range(5):
+        print value+"{0}.png".format(i)
+        img = cv2.imread("digits/"+value+"{0}.png".format(i))
 
-    # For every pixel in the image:
-    for x in range(height):
-        for y in range(width):
-            red = img[y, x, 2]
-            green = img[y, x, 1]
-            blue = img[y, x, 0]
-            if red == 255 and green == 255 and blue == 255:
-                pixels.append(1)
-            else:
-                pixels.append(0)
+        width = img.shape[0]
+        height = img.shape[1]
 
-    images.append((pixels, "0"))
+        pixels = []
 
-for i in range(5):
-    print "one{0}.png".format(i)
-    img = cv2.imread("digits/one{0}.png".format(i))
+        # For every pixel in the image:
+        for x in range(height):
+            for y in range(width):
+                red = img[y, x, 2]
+                green = img[y, x, 1]
+                blue = img[y, x, 0]
+                if red == 255 and green == 255 and blue == 255:
+                    pixels.append(1)
+                else:
+                    pixels.append(0)
 
-    width = img.shape[0]
-    height = img.shape[1]
-
-    pixels = []
-
-    # For every pixel in the image:
-    for x in range(height):
-        for y in range(width):
-            red = img[y, x, 2]
-            green = img[y, x, 1]
-            blue = img[y, x, 0]
-            if red == 255 and green == 255 and blue == 255:
-                pixels.append(1)
-            else:
-                pixels.append(0)
-
-    images.append((pixels, "1"))
+        images.append((pixels, digits[value]))
 
 def arrayOutput(img):
     img = cv2.imread(img)
@@ -79,4 +62,4 @@ def arrayOutput(img):
                 pixels.append(0)
     return pixels
 
-print classify(arrayOutput("classify/class1.png"), images)
+print classify(arrayOutput("classify/class3.png"), images)
